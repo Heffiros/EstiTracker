9701e7f31d9462e2bfb413d3cd7294344db78692
unknown <vasax.levy@gmail.com>
2015-11-24 15:20:29 +0100

    mise en place de l'architecture avec minima framework

---
+++ app/Controller/Misc.php
@@ -0,0 +1,12 @@
+<?php 
+namespace Controller;
+use Minima\Controller\Base;
+use \Model;
+
+class Misc extends Base
+{
+	public function homeAction()
+	{
+		
+	}
+}
---
+++ app/View/layout.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+    <title>EstiTracker</title>
+<link rel="stylesheet" type="text/css" href="<?php echo BASEPATH ?>vendor/bower/bootstrap/dist/css/bootstrap.css" />
+<meta charset="utf-8" />
+</head>
+<body>
+	<div class="row marketing">
+        <?php echo $data ?>
+    </div>
+</body>
+</html>
