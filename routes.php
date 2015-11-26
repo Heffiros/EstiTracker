6b8b348c90f95f16f0bd00ea57ed9dabe06d7d48
unknown <vasax.levy@gmail.com>
2015-11-26 20:19:43 +0100

    api 3/3

--- routes.php
+++ routes.php
@@ -2,7 +2,8 @@
 return array(
 	'' => 'Misc#home',
 	'backoffice' => 'Acceuil#home',
-	'api/connect' => 'Stat#connectApi',
-	'api/inscription' => 'Stat#createAccountApi',
-	'api/check' => 'Stat#checkEstiTypeApi'
+	'api/connect' => 'Api#connectApi',
+	'api/inscription' => 'Api#createAccountApi',
+	'api/check' => 'Api#checkEstiTypeApi',
+	'api/aff' => 'Api#affWebViewApiAction'
 );
