89b7f8822646fd9681ec92596a300aff713513a7
janvrot <antoine-janvrot@hotmail.fr>
2015-11-26 18:59:23 +0100

    navbar

--- app/View/layout.php
+++ app/View/layout.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8" />
   </head>
   <body>
-    <nav class="navbar navbar-trans navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-trans navbar-fixed-top" role="navigation" style="position:relative;">
       <div class="container">
 	<div class="navbar-header">
 	  <a class="navbar-brand text-danger" href="" style="color: #D6D325;">EstiTracker</a>
