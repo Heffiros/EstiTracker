11ae76977a3709d30f936f7d28038f32c4ae536f
LamaDelRay <salvetti3@live.fr>
2015-12-14 11:34:19 +0100

    Correction home

--- app/View/Misc/home.php
+++ app/View/Misc/home.php
@@ -3,7 +3,8 @@
 <div class="calque">
 </div>
 
-<div class="slider1" style="background : url(<?php echo BASEPATH?>app/Media/slide1.jpg) fixed no-repeat ; height : 100%;">
+<div class="slider1" style="background :url(<?php echo BASEPATH?>app/Media/slide1.jpg) fixed center no-repeat;
+background-color : #264151; height : 100%;">
 </div>
 
 <section class="container-fluid" id="section1">
